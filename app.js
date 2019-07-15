c0896ab6038a4484d37897a7efea408699551a0e
Lakshmi N <lakshminandhan138@gmail.com>
2019-07-15 19:58:39 +0530

    signup completed

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 let expressHBS=require('express-handlebars');
+
+let session=require('express-session');
+let passport=require('passport');
+let flash=require('connect-flash');
 let db=require('./dbconfig/db-connect');
 
 var indexRouter = require('./routes/index');
@@ -19,10 +23,15 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(session({secret:'mysecret',resave:false,saveUninitialized:false}));
+app.use(flash());
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
 
+app.use('/', indexRouter);
+//app.use('/user/signup',indexRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
